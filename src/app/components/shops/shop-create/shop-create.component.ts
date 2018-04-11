2f85f8b0c73043ce714edba6d02e66f270f10ccf
Cristina Bertrand <cristina@11870.com>
2018-04-12 00:44:59 +0200

    feat:shop create without image and specs

--- src/app/components/shops/shop-create/shop-create.component.ts
+++ src/app/components/shops/shop-create/shop-create.component.ts
@@ -1,18 +1,28 @@
+import { ShopsService } from './../../../shared/services/shops.service';
+import { Shop } from './../../../shared/model/shop.model';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shop-create',
   templateUrl: './shop-create.component.html',
   styleUrls: ['./shop-create.component.css']
 })
-export class ShopCreateComponent implements OnInit {
+export class ShopCreateComponent {
+  shop: Shop = new Shop();
 
   constructor(
     private router: Router,
+    private shopsService: ShopsService
   ) { }
 
-  ngOnInit() {
-  }
+  onSubmitShop(shopForm: NgForm) {
 
-}
+      this.shopsService.create(this.shop)
+        .subscribe((phone) => {
+          shopForm.reset();
+          this.router.navigate(['/shops']);
+        });
+    }
+  }
